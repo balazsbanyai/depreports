1a4b432af138f85d2350e422d05869db80f5dc6f
Balazs Banyai <balazs.banyai@logmein.com>
2016-12-11 15:14:18 +0100

    create dirs & files

---
+++ src/main/groovy/com/banyaibalazs/depreport/DepReportPlugin.groovy
@@ -0,0 +1,15 @@
+package com.banyaibalazs.depreport
+
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+
+class DepReportPlugin implements Plugin<Project> {
+
+
+   void apply(Project project) {
+
+
+   }
+}
